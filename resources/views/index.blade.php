d285395658e1dd418cdd253b608c0411cf211c79
Mysfer <jpfreitas2001@gmail.com>
2019-09-15 01:10:33 -0300

    carousel inicial responsivel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,21 +9,21 @@ $count = 0;
 
   <div align="center" style="color:#b40404"><h1>Comércios em destaque</h1></div>
   <br><br>
-  <div class="row">
-    <div class="col-md-12">
-      <div class="carousel slide multi-carousel-item" id="theCarousel" data-ride="carousel"> 
+
+  <div class="row multi-slider">
+    <div class="col-md-12 col-lg-12 ">
+      <div class="carousel carousel-showmanymoveone slide multi-item-carousel" id="theCarousel">
         <div class="carousel-inner">
           
-        @foreach($dados as $d)
-          <div class="item{{$count == 0 ? ' active' : '' }}">
+            @foreach($dados as $d)
+            <div class="item{{$count == 0 ? ' active' : '' }}">
+            <?php 
+            $nomee = str_replace(' ', '-', $d->nome);
+            ?>
+                <div class="col-xs-12 col-sm-6 col-md-4"><a href="/Detalhes/Destaques/{{$d->id}}/{{$nomee}}"><img src="{{$d->banner}}" class="img" width="100%" alt="{{$d->nome}}"></a></div> <?php $count++;?> 
+            </div>
+            @endforeach
 
-          <?php 
-          $nomee = str_replace(' ', '-', $d->nome);
-          ?>
-
-            <div class="col-xs-4"><a href="/Detalhes/Destaques/{{$d->id}}/{{$nomee}}"><img src="{{$d->banner}}" class="img" width="100%"></a></div> <?php $count++;?>
-          </div>
-        @endforeach
         </div>
         <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
         <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
@@ -31,6 +31,9 @@ $count = 0;
     </div>
   </div>
 
+    
+  
+  
 
   
  
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -10,10 +10,9 @@
                 <link rel="icon" href="favicon.ico" type="image/x-icon">
                 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
                 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700" rel="stylesheet">
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" /><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css" /><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
                 <link rel="stylesheet" href="{{ URL::asset('css/ligmogi.css') }}" >
                 <link rel="shortcut icon" href="{{URL::asset('image/fav')}}" />
-                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
                 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
                 
@@ -54,9 +53,6 @@
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 wrapperPesquisa">
     
                     <div class="row tiposPesquisa">
-    
-                        <span class="loading"><img src="img/carregando.gif" alt="carregando..." title="carregando..." width="100" height="31" style="float: right;"></span>
-    
                         <!-- nome -->
                         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 wrapperTab">
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 tabs tabNome tabAtiva">
@@ -125,9 +121,9 @@
      
     </div> <!-- ./container -->
     
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
     
         <script>
             var pagina = false;
